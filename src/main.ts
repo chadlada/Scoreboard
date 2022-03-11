cbfaedd2ee488bc49568568e4b3c101ee9b09755
chadlada <chadlada@gmail.com>
2022-03-11 14:08:00 -0500

    Found elements for team one

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import './style.css'
+
+// let teamOneScore = 0
+
+const teamOnePlus = document.querySelector('.team1 i.add')
+const teamOneMinus = document.querySelector('.team1 i.subtract')
+const teamOneScore = document.querySelector('.team1 h3')
+const teamOneNameDisplay = document.querySelector('.team1 h2')
+const teamOneNameInput = document.querySelector('team1 input')
+
+console.log(teamOnePlus)
+console.log(teamOneMinus)
+console.log(teamOneScore)
+console.log(teamOneNameDisplay)
+console.log(teamOneNameInput)
