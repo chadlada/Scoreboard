46db4572bf52ad3a744ec3c7267a29be0184ae87
chadlada <chadlada@gmail.com>
2022-03-14 11:39:08 -0400

    Added 21 score limit

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,27 @@ console.clear()
 const teamOnePlusButton = document.querySelector('.team1 i.add')
 const teamOneMinusButton = document.querySelector('.team1 i.subtract')
 const teamOneScoreText = document.querySelector('.team1 h3')
-// const teamOneNameDisplay = document.querySelector('.team1 h2')
-// const teamOneNameInput = document.querySelector('team1 input')
+const teamOneNameInput = document.querySelector('.team1 input')
+const teamOneNameText = document.querySelector('.team1 h2')
+
 let teamOneScore = 0
 
 const teamTwoPlusButton = document.querySelector('.team2 i.add')
 const teamTwoMinusButton = document.querySelector('.team2 i.subtract')
 const teamTwoScoreText = document.querySelector('.team2 h3')
-// const teamTwoNameDisplay = document.querySelector('.team2 h2')
-// const teamTwoNameInput = document.querySelector('team2 input')
+const teamTwoNameInput = document.querySelector('.team2 input')
+const teamTwoNameText = document.querySelector('.team2 h2')
+
 let teamTwoScore = 0
 
-// ------------------------------------------TEAM 1------------------------------------------
+// ------------------------------------------TEAM 1 BUTTON FUNCTION------------------------------------------
 
 function handleClickTeamOnePlusButton() {
   teamOneScore++
+
+  if (teamOneScore > 21) {
+    return
+  }
 
   if (teamOneScoreText) {
     teamOneScoreText.textContent = `${teamOneScore}`
@@ -43,9 +49,29 @@ function handleClickTeamOneMinusButton() {
 
 teamOneMinusButton?.addEventListener('click', handleClickTeamOneMinusButton)
 
-// ------------------------------------------TEAM 2------------------------------------------
+// ------------------------------------------TEAM 1 INPUT FUNCTION------------------------------------------
+
+function teamOneInputChanged(event: Event) {
+  const inputThatChanged = event.target
+
+  if (inputThatChanged instanceof HTMLInputElement) {
+    const textThatWasInput = inputThatChanged.value
+
+    if (teamOneNameText) {
+      teamOneNameText.textContent = textThatWasInput
+    }
+  }
+}
+
+teamOneNameInput?.addEventListener('input', teamOneInputChanged)
+
+// ------------------------------------------TEAM 2 BUTTON FUNCTION------------------------------------------
 
 function handleClickTeamTwoPlusButton() {
+  if (teamTwoScore >= 21) {
+    return
+  }
+
   teamTwoScore++
 
   if (teamTwoScoreText) {
@@ -68,3 +94,19 @@ function handleClickTeamTwoMinusButton() {
   console.log(teamTwoScore)
 }
 teamTwoMinusButton?.addEventListener('click', handleClickTeamTwoMinusButton)
+
+// ------------------------------------------TEAM 2 INPUT FUNCTION------------------------------------------
+
+function teamTwoInputChanged(event: Event) {
+  const inputThatChanged = event.target
+
+  if (inputThatChanged instanceof HTMLInputElement) {
+    const textThatWasInput = inputThatChanged.value
+
+    if (teamTwoNameText) {
+      teamTwoNameText.textContent = textThatWasInput
+    }
+  }
+}
+
+teamTwoNameInput?.addEventListener('input', teamTwoInputChanged)
